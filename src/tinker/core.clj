5a919c93324d0fe3d860fbc15879c119d260ab45
Maximilian A. Giraldo <maxagiraldo@gmail.com>
2019-12-04 08:09:27 -0500

    feat: add murders dataset and add data fn
    Include a dataset for learning and testing purposes.
    
    Also create an initial implementation of `data` fn, whose job
    is to load data, e.g. a CSV file, into something akin to a dataframe.

--- src/tinker/core.clj
+++ src/tinker/core.clj
@@ -1,6 +1,18 @@
-(ns tinker.core)
+(ns tinker.core
+  (:require 
+    [clojure.java.io :as io]
+    [clojure.data.csv :as csv]))
 
 (defn data 
   "loads in csv file from data/ dir"
   [dataset]
-  dataset)
+  (let [valid-file-exts ["csv" "CSV"]
+        dataset-name (name dataset)]
+    (with-open [reader (io/reader (str "data/" dataset-name ".csv"))]
+      (doall
+        (csv/read-csv reader)))))
+
+(def murders (data :murders))
+;; TODO: accept various file extensions, e.g. csv, CSV
+;; TODO: returned murders is properly formatted
+
